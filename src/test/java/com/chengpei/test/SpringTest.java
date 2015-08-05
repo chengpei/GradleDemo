01a43f7cd54ea7940b23a0584605077e6bb7cbb8
chengpei <472799208@qq.com>
2015-08-05 23:09:40 +0800

    Quartz Demo

--- src/test/java/com/chengpei/test/SpringTest.java
+++ src/test/java/com/chengpei/test/SpringTest.java
@@ -1,9 +1,5 @@
 package com.chengpei.test;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 /**
  * Created by chengpei on 2015/8/5.
  */
