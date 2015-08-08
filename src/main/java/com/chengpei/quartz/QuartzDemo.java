4d2e62b60fa27ae8607a1d455d9e63f270b813f2
chengpei <472799208@qq.com>
2015-08-08 17:08:10 +0800

    Quartz 触发器Demo

--- src/main/java/com/chengpei/quartz/QuartzDemo.java
+++ src/main/java/com/chengpei/quartz/QuartzDemo.java
@@ -32,7 +32,7 @@ public class QuartzDemo {
 
         // 创建一个SimpleTrigger实例，指定该Trigger在Scheduler中所属组及名称。
         // 接着设置调度的时间规则.当前时间运行
-        Trigger trigger = newTrigger().withIdentity("trigger1", "group12")
+        Trigger trigger = newTrigger().withIdentity("trigger1", "group1")
 //                .withSchedule(simpleSchedule().withIntervalInSeconds(5).repeatForever())
                 .withSchedule(cronSchedule("0/5 * * * * ?"))
                 .startNow().build();
@@ -40,19 +40,35 @@ public class QuartzDemo {
         // 注册并进行调度
         scheduler.scheduleJob(job, trigger);
 
-        // 启动调度器
-        scheduler.start();
+        if(scheduler.isStarted()){
+            //调度器停止运行
+            scheduler.shutdown(true);
+        }else{
+            // 启动调度器
+            scheduler.start();
+        }
 
         try {
-            //当前线程等待65秒
-            Thread.sleep(45L * 1000L);
+            //当前线程等待15秒
+            Thread.sleep(15L * 1000L);
         } catch (Exception e) {
 
         }
 
-        //调度器停止运行
-        scheduler.shutdown(true);
+        //获取另一个调度者
+        Scheduler scheduler2 = sf.getScheduler();
 
-        System.out.println("结束运行。。。。");
+        //根据触发器名字和组 获取触发器key
+        TriggerKey triggerKey = new TriggerKey("trigger1", "group1");
+        //根据触发器key 找到触发器
+        CronTriggerImpl trigger2 = (CronTriggerImpl) scheduler.getTrigger(triggerKey);
+
+//        JobKey jobKey = new JobKey("job1", "group1");
+//        JobDetail job2 = scheduler2.getJobDetail(jobKey);
+        //覆盖原有的cron表达式
+        trigger2.setCronExpression("0/1 * * * * ?");
+
+        // 重新加载触发器cron
+        scheduler2.rescheduleJob(triggerKey, trigger2);
     }
 }
